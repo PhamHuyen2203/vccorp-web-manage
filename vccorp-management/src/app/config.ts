fc596e541259c8eae56ea396012f84d6ecdefb2c
PhamHuyen2203 <huyenpt23411@st.uel.edu.vn>
2026-05-29 10:12:29 +0700

    Update frontend API_URL to point to production backend

--- vccorp-management/src/app/config.ts
+++ vccorp-management/src/app/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
-// export const API_URL = 'https://vccorp-backend.vercel.app';
+// export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://vccorp-backend.vercel.app';
